5c197ca92c10c88f8f281e42038d17fafe1f8ec0
contrazap <contrazap@gmail.com>
2023-03-22 00:18:25 +0530

    Added onClick event

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -7,7 +7,13 @@ function ListGroup() {
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item) => (
-          <li className="list-group-item" key={item}>
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => {
+              console.log(item + " Clicked");
+            }}
+          >
             {item}
           </li>
         ))}
