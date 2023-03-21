1d98bf1522f97ce35b0b90a49cd694df53706949
contrazap <contrazap@gmail.com>
2023-03-22 00:00:33 +0530

    conditional element using ternary operator

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,13 @@
 function ListGroup() {
-  const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  items = [];
+
+  const message = items.length === 0 ? <p>No item found</p> : null;
 
   return (
     <>
       <h1>List</h1>
+      {message}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
