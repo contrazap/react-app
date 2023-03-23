92aa8c84b4ab186a1eddfed0058d4f57cf72f145
contrazap <contrazap@gmail.com>
2023-03-23 14:08:55 +0530

    Update a list item by updating state

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
 import { useState } from "react";
-import Alert from "./components/Alert";
-import ListGroup from "./components/ListGroup";
-import Button from "./components/Button";
-import Like from "./components/Like";
 
 function App() {
-  const [alertVisible, setAlertVisibility] = useState(false);
-  let items = ["New York", "San Diego", "Tampa"];
+  const [game, setGame] = useState({
+    id: 1,
+    player: {
+      name: "John",
+    },
+  });
+
+  const handleGame = () => {
+    setGame({
+      ...game,
+      player: {
+        name: "Bob",
+      },
+    });
+  };
 
   return (
     <div>
-      {alertVisible && (
-        <Alert onClose={() => setAlertVisibility(false)}>Alert</Alert>
-      )}
-      <Like onClick={() => console.log("clicked")} />
-      {items.length === 0 && <p>No item found</p>}
-      <ListGroup
-        heading="Cities"
-        items={items}
-        // onSelectItem={() => console.log("Clicked")}
-      ></ListGroup>
-      <Button color="success" onClick={() => setAlertVisibility(true)}>
-        Submit
-      </Button>
+      {game.player.name} <br /> <button onClick={handleGame}>Update</button>
     </div>
   );
 }
