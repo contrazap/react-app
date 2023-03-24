b4f426f6749d0a0271183dbb9bc39fd2ad52a6a4
contrazap <contrazap@gmail.com>
2023-03-24 08:14:19 +0530

    update value inside an object which is inside another object with state hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
 import { useState } from "react";
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
 
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: 2 } : { ...item }
+      ),
+    });
   };
 
   return (
     <div>
-      <li>{pizza.name}</li>
-      {pizza.toppings.map((item) => (
-        <li>{item}</li>
+      <li>{cart.discount}</li>
+      {cart.items.map((item) => (
+        <>
+          <li>title: {item.title}</li>
+          <li>quantity: {item.quantity}</li>
+        </>
       ))}
       <button onClick={handleClick}>Update</button>
     </div>
