abffade5bbe83172883fa68263722bd4ad422f77
contrazap <contrazap@gmail.com>
2023-03-26 13:35:08 +0530

    Used axios to send a get request to json placeholder api

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
-import { useState } from "react";
-import ProductList from "./components/ProductList";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
 
 function App() {
-  const [category, setCategory] = useState("");
+  const [users, setUsers] = useState<User[]>();
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    axios
+      .get<User[]>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setUsers(res.data))
+      .catch((err) => setError(err.message));
+  }, []);
 
   return (
-    <div>
-      <select
-        className="form-select"
-        onChange={(event) => setCategory(event.target.value)}
-      >
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-      </select>
-      <ProductList category={category} />
-    </div>
+    <>
+      {error && <p className="text-danger">{error}</p>}
+      <ul>
+        {users && users.map((user) => <li key={user.id}>{user.name}</li>)}
+      </ul>
+    </>
   );
 }
 
